3b64973aa8654fd0b9f47740d6a74c94f3281a24
Sneha kumari <55735336+snehasharma76@users.noreply.github.com>
2022-08-21 01:04:29 +0530

    Time: 14 ms (82.93%), Space: 5.9 MB (74.72%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        long long result = 0;
-        long long temp = x;
+       long long  result = 0;
+        int temp = 0;
+        temp = x;
         if(x<0)return false;
-        while(x>0){
-            long long rem = x%10;
-            result = (result*10)+rem;
+        while(x!=0){
+            long long digit = x%10;
+            result = (result*10)+digit;
             x = x/10;
+            //temp = rem;
         }
-        //cout << temp << " " << result;
-        if(temp == result)return true;
-        return false;
-        
+        return (temp==result);
     }
 };
